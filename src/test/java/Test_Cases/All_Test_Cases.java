618b67e272a08f96fe01afb2e379b12f79a9d84f
purno <pg.purnoghosh@gmail.com>
2025-02-22 10:17:54 +0600

    add mail

--- src/test/java/Test_Cases/All_Test_Cases.java
+++ src/test/java/Test_Cases/All_Test_Cases.java
@@ -7,20 +7,20 @@ import java.io.IOException;
 
 public class All_Test_Cases extends Setup {
 
-    @Test(priority = 1,enabled = true)
-    public void Bangladeshi_Employees_Attendance() throws IOException, InterruptedException {
-        Bangladeshi_Employee_Attendance actions = new Bangladeshi_Employee_Attendance(driver);
-        String Actual_value= actions.SingleEmployeeAttendance();
-
-    }
-
-
-//    @Test(priority = 2)
-//    public void Multiple_Employees_Attendance() throws IOException, InterruptedException {
-//        Multiple_Employee_Attendance action2=new Multiple_Employee_Attendance(driver);
-//        action2.multipleEmployeeAttendance();
+//    @Test(priority = 1,enabled = true)
+//    public void Bangladeshi_Employees_Attendance() throws IOException, InterruptedException {
+//        Bangladeshi_Employee_Attendance actions = new Bangladeshi_Employee_Attendance(driver);
+//        String Actual_value= actions.SingleEmployeeAttendance();
 //
 //    }
+
+
+    @Test(priority = 2)
+    public void Multiple_Employees_Attendance() throws IOException, InterruptedException {
+        Multiple_Employee_Attendance action2=new Multiple_Employee_Attendance(driver);
+        action2.multipleEmployeeAttendance();
+
+    }
 
 
 }
